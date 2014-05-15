62badc8962d4db5e0cb34c1247dc23a903e62148
paul <paul@quant>
2014-05-15 14:26:32 -0400

    committing Constants.java

---
+++ src/cards/Constants.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cards;
+
+/**
+ *
+ * @author paul
+ */
+public class Constants {
+    public static final double ROYAL_FLUSH_PROB = .000032;
+    public static final double STRAIGHT_FLUSH_PROB = .00029;
+    public static final double FOUR_OF_A_KIND_PROB = .0018;
+    public static final double FULL_HOUSE_PROB = .026;
+    public static final double FLUSH_PROB = .0303;
+    public static final double STRAIGHT_PROB = .0462;
+    public static final double THREE_OF_A_KIND_PROB =.0483;
+    public static final double TWO_PAIRS_PROB = .235;
+    public static final double ONE_PAIR_PROB = .438;
+    public static final double HIGH_CARD_PROB = .174;
+}
